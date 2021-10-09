63e21f491f4a59a6d69a4b65fec3883cdb3b0900
naveen3232t <44412641+naveen3232t@users.noreply.github.com>
2021-10-09 00:17:44 -0500

    Update PastaFactory.java

--- PastaMachine/src/PastaFactory.java
+++ PastaMachine/src/PastaFactory.java
@@ -1,6 +1,6 @@
 public class PastaFactory {
     public static void main(String args[]){
-        PastaMachine p = new PastaMachineBuilder().setBaseflour("null").setPizzatype("null").setShape("null").setSize(0).setPizzacount(0);
+        PastaMachine p = new PastaMachineBuilder().setBaseflour("").setPizzatype("").setShape("").setSize(0).setPizzacount(0);
         System.out.println(p);
 
 
